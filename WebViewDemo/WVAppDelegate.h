ae23cbb5c3bdd8b912a7a216184b440d2ab8a20a
Nirav Savjani <nsavjani@gmail.com>
2013-10-28 21:52:59 -0700

    Initial Commit

---
+++ WebViewDemo/WVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WVAppDelegate.h
+//  WebViewDemo
+//
+//  Created by Nirav Savjani on 10/28/13.
+//  Copyright (c) 2013 Nirav Savjani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebViewDemo
+//
+//  Created by Nirav Savjani on 10/28/13.
+//  Copyright (c) 2013 Nirav Savjani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WVAppDelegate class]));
+    }
+}
